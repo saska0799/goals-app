e691a99e6d5600ac6a4a63a1a53ab370a1a7c59b
Aleksandra Marinovic <63455453+saska0799@users.noreply.github.com>
2023-03-22 13:08:37 +0100

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,18 +1,25 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const goalsRoutes = require("./routes/goals");
 const userRoutes = require("./routes/users");
 
 const app = express();
+const PORT = process.env.PORT || 3030;
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use("/api/goals", goalsRoutes);
 app.use("/api/users", userRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => app.listen(process.env.PORT, () => console.log("connected")))
+  .then(() => app.listen(PORT, () => console.log("connected")))
   .catch((err) => console.log(err));
